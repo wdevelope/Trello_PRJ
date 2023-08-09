329685e8d33ccbf023a842784f798365dd9e8164
Lee seongwon <jsw6357@naver.com>
2023-08-09 19:44:24 +0900

    feat: column 프론트 백 연결

--- src/public/index.js
+++ src/public/index.js
@@ -1,3 +1,7 @@
+const { response } = require("express");
+$(document).ready(function () {
+  getColumn();
+});
 // 회원가입 함수
 function signup() {
   const email = document.getElementById("registerEmail").value;
@@ -46,6 +50,80 @@ function login() {
         // 추가적으로 토큰 저장, 페이지 이동 등의 동작을 수행할 수 있습니다.
       } else {
         alert("로그인 실패: " + data.message);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+//컬럼 조회(상시)
+function getColumn() {
+  fetch(`http://localhost:3000/board/${boardId}/column`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        const columns = data.columns; // 서버에서 받은 컬럼 정보
+        const mainBoard = document.getElementById("mainBoard");
+
+        columns.forEach((column) => {
+          const { title, position } = column;
+
+          const columnDiv = document.createElement("div");
+          columnDiv.className = "col-lg-3 col-md-6 col-sm-12 mb-4";
+
+          const columnEl = document.createElement("div");
+          columnEl.className = "column h-100 p-2 bg-light rounded";
+
+          const h4 = document.createElement("h4");
+          h4.textContent = title;
+
+          const cardDiv = document.createElement("div");
+          cardDiv.className = "card mb-2 p-2 border rounded";
+
+          const cardButton = document.createElement("button");
+          cardButton.className = "btn btn-secondary mt-3";
+          cardButton.textContent = "카드 추가";
+
+          cardDiv.appendChild(cardButton);
+          columnEl.appendChild(h4);
+          columnEl.appendChild(cardDiv);
+          columnDiv.appendChild(columnEl);
+
+          mainBoard.appendChild(columnDiv);
+        });
+      } else {
+        alert("컬럼 조회 실패: " + data.message);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+//컬럼 추가
+function createColumn() {
+  const title = document.getElementById("columnTitle").value;
+  const position = document.getElementById("columnPosition").value;
+  console.log(title, position);
+
+  fetch(`http://localhost:3000/board/${boardId}/column`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title, position }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        alert("컬럼 생성 성공");
+      } else {
+        alert("컬럼 생성 실패: " + data.message);
       }
     })
     .catch((error) => {
