85f0a06c7e4fa8ec810b24c3dab3a72ddbc18db6
HyungJin0114 <spunkup20@naver.com>
2023-08-09 19:52:50 +0900

    보드등록

--- src/public/index.js
+++ src/public/index.js
@@ -144,4 +144,35 @@ function addCard(boardId, columnId) {
     });
 }
 
+//보드 등록 함수
+function createBoard(){
+  const title = document.getElementById("titleBoard").value;
+  const description = document.getElementById("descriptionBoard").value;
+  const color = document.getElementById("colorBoard").value;
+  alert(title, description, color);
+  console.log(title, description, color);
+
+  console.log(title, description, color);
+  fetch("http://localhost:3000/board", {   /*로컬에 userId 가져와야하는데?! */
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: sessionStorage.getItem('Authorization'),
+    },
+    body: JSON.stringify({ title, description, color }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        alert("보드 등록 완료");
+      } else {
+        alert("보드 등록 실패: " + data.message);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+}
+
 
