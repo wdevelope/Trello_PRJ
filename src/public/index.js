5ab4b91fa9ec2c74d6eeeb4c27ad23a4730a6dda
HyungJin0114 <spunkup20@naver.com>
2023-08-09 19:43:15 +0900

    프론트 시작

--- src/public/index.js
+++ src/public/index.js
@@ -53,6 +53,40 @@ function login() {
     });
 }
 
+//보드 등록 함수
+function createBoard(){
+  const title = document.getElementById("titleBoard").value;
+  const description = document.getElementById("descriptionBoard").value;
+  const color = document.getElementById("colorBoard").value;
+  alert(title, description, color);
+  console.log(title, description, color);
+
+  console.log(title, description, color);
+  fetch("http://localhost:3000/board", {   /*로컬에 userId 가져와야하는데?! */
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: sessionStorage.getItem('Authorization'),
+    },
+    body: JSON.stringify({ title, description, color }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        alert("보드 등록 완료");
+      } else {
+        alert("보드 등록 실패: " + data.message);
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+}
+
 // 이벤트 리스너 등록 (가정: 각각의 버튼의 id가 'signupButton'과 'loginButton'임)
 document.getElementById("signupButton").addEventListener("click", signup);
 document.getElementById("loginButton").addEventListener("click", login);
+
+// 이벤트 리스너 등록
+document.getElementById("createBoardButton").addEventListener("click", createBoard);
