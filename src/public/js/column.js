933c3e88dbda8ac18a5f35d6dda43bfad59eebee
Lee seongwon <jsw6357@naver.com>
2023-08-10 14:15:12 +0900

    feat:column.js 폴더 패치함수를 이용하여 컬럼  CRUD 기능 생성

--- src/public/js/column.js
+++ src/public/js/column.js
@@ -1,74 +1,181 @@
-//컬럼 조회(상시)
-function getColumn() {
-  fetch(`http://localhost:3000/board/${boardId}/column`, {
+// 페이지 로드 시 실행될 함수
+document.addEventListener("DOMContentLoaded", loadColumn);
+//컬럼 조회
+async function loadColumn(boardId) {
+  const option = {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
+      Authorization: localStorage.getItem("Authorization"),
     },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.success) {
-        const columns = data.columns; // 서버에서 받은 컬럼 정보
-        const mainBoard = document.getElementById("mainBoard");
+  };
 
-        columns.forEach((column) => {
-          const { title, position } = column;
+  const column = (
+    await fetch(`http://localhost:3000/board/${boardId}/column`, option).then(
+      (d) => d.json(),
+    )
+  ).data;
 
-          const columnDiv = document.createElement("div");
-          columnDiv.className = "col-lg-3 col-md-6 col-sm-12 mb-4";
+  const container = document.getElementById("mainBoard");
+  container.innerHTML = "";
+  column.forEach((data) => {
+    const columnDiv = document.createElement("div");
+    columnDiv.classList.add("col-lg-3", "col-md-6", "col-sm-12", "mb-4");
 
-          const columnEl = document.createElement("div");
-          columnEl.className = "column h-100 p-2 bg-light rounded";
+    columnDiv.innerHTML += `
+    <div class="col-lg-3 col-md-6 col-sm-12 mb-4">
+    <div class="column h-100 p-2 bg-light rounded">
+      <h4>${data.title}</h4>
+      
+      <button
+        data-toggle="modal"
+        data-target="#addCardModal"
+        class="btn btn-secondary mt-3"
+      >
+        카드 추가
+      </button>
+    </div>
+  </div>
 
-          const h4 = document.createElement("h4");
-          h4.textContent = title;
+  <div class="modal fade" id="addCardModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">카드 추가</h4>
+        <button type="button" class="close" data-dismiss="modal">
+          &times;
+        </button>
+      </div>
+      <div class="modal-body">
+        <label for="cardTitle">카드 제목</label>
+        <input
+          type="text"
+          id="cardTitle"
+          placeholder="카드 제목"
+          class="form-control mb-2"
+        />
 
-          const cardDiv = document.createElement("div");
-          cardDiv.className = "card mb-2 p-2 border rounded";
+        <label for="cardContent">카드 내용</label>
+        <textarea
+          id="cardContent"
+          placeholder="카드 내용을 입력하세요."
+          class="form-control mb-2"
+          rows="3"
+        ></textarea>
 
-          const cardButton = document.createElement("button");
-          cardButton.className = "btn btn-secondary mt-3";
-          cardButton.textContent = "카드 추가";
+        <label for="dueDate">마감일</label>
+        <input
+          type="date"
+          id="dueDate"
+          placeholder="마감일"
+          class="form-control mb-2"
+        />
 
-          cardDiv.appendChild(cardButton);
-          columnEl.appendChild(h4);
-          columnEl.appendChild(cardDiv);
-          columnDiv.appendChild(columnEl);
+        <label for="cardColor">색깔</label>
+        <input type="color" id="cardColor" class="form-control mb-2" />
 
-          mainBoard.appendChild(columnDiv);
-        });
-      } else {
-        alert("컬럼 조회 실패: " + data.message);
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+        <label for="cardPosition">순서</label>
+        <input
+          type="number"
+          id="cardPosition"
+          placeholder="순서 (예: 1)"
+          class="form-control mb-2"
+        />
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary"onclick="addCard()">
+          추가
+        </button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">
+          닫기
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+            `;
+    container.appendChild(columnDiv);
+  });
 }
 
-//컬럼 추가
-function createColumn() {
-  const title = document.getElementById("columnTitle").value;
-  const position = document.getElementById("columnPosition").value;
-  console.log(title, position);
+//컬럼 생성
+async function addColumn(boardId) {
+  const obj = {};
+  obj.title = $("#columnTitle").val();
+  obj.position = $("#columnPosition").val();
 
-  fetch(`http://localhost:3000/board/${boardId}/column`, {
+  const option = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Authorization: localStorage.getItem("Authorization"),
     },
-    body: JSON.stringify({ title, position }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.success) {
-        alert("컬럼 생성 성공");
-      } else {
-        alert("컬럼 생성 실패: " + data.message);
-      }
-    })
-    .catch((error) => {
-      console.error("Error:", error);
+
+    body: JSON.stringify(obj),
+  };
+
+  try {
+    const fetchedData = await fetch(
+      `http://localhost:3000/board/${boardId}/column`,
+      option,
+    ).then((data) => {
+      return data.json();
     });
+    console.log(fetchedData);
+    location.reload();
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+//컬럼 수정
+async function updateColumn(boardId, columnId) {
+  const obj = {};
+  obj.title = $("#columnTitle").val();
+  obj.position = $("#columnPosition").val();
+
+  const option = {
+    method: PUT,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("Authorization"),
+    },
+    body: JSON.stringify(obj),
+  };
+
+  try {
+    const fetchedData = await fetch(
+      `http://localhost:3000/board/${boardId}/column/${columnId}`,
+      option,
+    ).then((data) => {
+      return data.json();
+    });
+    console.log(fetchedData);
+    location.reload();
+  } catch (e) {
+    console.error(e);
+  }
+}
+//컬럼 삭제
+async function deleteColumn(boardId, columnId) {
+  const option = {
+    method: DELETE,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: localStorage.getItem("Authorization"),
+    },
+  };
+
+  try {
+    const fetchedData = await fetch(
+      `http://localhost:3000/board/${boardId}/column/${columnId}`,
+      option,
+    ).then((data) => {
+      return data.json();
+    });
+    console.log(fetchedData);
+    location.reload();
+  } catch (e) {
+    console.error(e);
+  }
 }
