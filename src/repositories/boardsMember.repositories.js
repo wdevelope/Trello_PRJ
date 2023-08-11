2bea967d7ad6aea799a98443a24c980d6d2858f0
HyungJin0114 <spunkup20@naver.com>
2023-08-11 14:33:13 +0900

    boardMember 맴버추가, 보드삭제, 보드맴버조회

--- src/repositories/boardsMember.repositories.js
+++ src/repositories/boardsMember.repositories.js
@@ -2,20 +2,36 @@ const boardMember = require("../dataBase/models/boardMember");
 const { Op } = require("sequelize");
 
 class BoardsMemberRepository {
-
   //보드 맴버 추가
-  async createBoardMember(boardId, userId) {
-    console.log("repo userId", userId);
+  async createBoardMember(memberId, boardId) {
     console.log("repo boardId", boardId);
-    return boardMember.create({ boardId, userId, });
-  };
-  
+    console.log("repo memberId", memberId);
+    const userId = memberId;
+
+    return boardMember.create({ userId, boardId });
+  }
+
   //보드 맴버 삭제
-  async deleteBoardMember(MemberId, boardId){
+  async deleteBoardMember(memberId, boardId) {
     // 보드 맴버 안에 특정 맴버찾고 삭제
     return await boardMember.destroy({
-      where: {[Op.and]:[{id: MemberId},{boardId}]},
+      where: { [Op.and]: [{ userId: memberId }, { boardId }] },
     });
+  }
+
+  //보드 맴버 삭제 권한 확인
+  //   async compareBoardMamber(boardId){
+  //     const compareBoardMamberData = await boardMember.findOne({
+  //       attributes: ["userId"],
+  //       where: { id: boardId },
+  //     });
+  //     return compareBoardData;
+  //   }
+
+  //보드 맴버 조회
+  async getBoardMember(boardId) {
+    console.log("repo", boardId);
+    return boardMember.findAll({ where: { boardId } });
   }
 }
 
--- src/services/boardsMember.service.js
+++ src/services/boardsMember.service.js
@@ -3,29 +3,54 @@ const boardsMemberRepository = new BoardsMemberRepository();
 
 class BoardsService {
   //보드 맴버 추가
-  async createBoardMember(boardId, userId) {
+  async createBoardMember(memberId, boardId, userId) {
     console.log("ser userId", userId);
     console.log("ser boardId", boardId);
+    console.log("ser memberId", memberId);
+
     const newBoardMember = await boardsMemberRepository.createBoardMember(
+      memberId,
       boardId,
-      userId,
     );
     return newBoardMember;
   }
 
   //보드 맴버 삭제
-  async deleteBoardMember(MemberId, boardId, userId){
-    console.log("ser MemberId", MemberId);
+  async deleteBoardMember(memberId, boardId, userId) {
+    console.log("ser memberId", memberId);
     console.log("ser boardId", boardId);
     console.log("ser userId", userId);
 
     //comparBoardMemberData 따로 만들어서 boardId == userId가 같은지 비교
+    // const compareBoardMamberData =
+    //   await boardsMemberRepository.compareBoardMamber(boardId);
+    // if (!compareBoardMamberData) {
+    //   throw new Error("보드 맴버 삭제 권한 없음");
+    // }
+    // if (compareBoardMamberData.userId !== userId) {
+    //   throw new Error("보드 맴버 삭제 권한 없음");
+    // }
 
-    const deleteBoardMember = await boardsMemberRepository.deleteBoardMember(MemberId, boardId);
-    if(!deleteBoardMember){
-        throw new Error("삭제할 보드 맴버가 없습니다.");
+    const deleteBoardMember = await boardsMemberRepository.deleteBoardMember(
+      memberId,
+      boardId,
+    );
+    if (!deleteBoardMember) {
+      throw new Error("삭제할 보드 맴버가 없습니다.");
     }
     return deleteBoardMember;
+  }
+
+  //보드 맴버 조회
+  async getBoardMember(boardId) {
+    console.log("ser boardId", boardId);
+    const getBoardMember = await boardsMemberRepository.getBoardMember(boardId);
+
+    if (!boardId) {
+      throw new Error("보드 맴버가 없습니다.");
+    }
+
+    return getBoardMember;
   }
 }
 
